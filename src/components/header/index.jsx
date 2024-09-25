5e5f01f76e811c0623cd6a00c0ac6a22cba5773a
Oleksandr Zhurba <a.zhurbinho@gmail.com>
2024-09-25 11:45:28 +0200

    fix: added styles to ul navbar

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,8 +1,10 @@
+import Navbar from "../navbar";
 import styles from "./styles.module.css";
 function Header() {
   return (
     <header className={styles.header_container}>
       <h1>Sneakers - shop</h1>
+      <Navbar />
     </header>
   );
 }
